c4a92aaa9b6eed07b98b55419e4feb96a7daeb31
Mark Stiller <mstiller1@ggc.edu>
2020-02-07 10:39:11 -0500

    2d and 3d animation work

---
+++ 3DAnimation/Assets/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Animator myAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        myAnim.SetFloat("HAXIS", h);
+        myAnim.SetFloat("VAXIS", v);
+    }
+}
---
+++ Mario2D/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Animator myAnim;
+    SpriteRenderer myRend;
+    Rigidbody2D myBod;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnim = GetComponent<Animator>();
+        myRend = GetComponent<SpriteRenderer>();
+        myBod = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+        if (h > 0)
+        {
+            myAnim.SetBool("Run", true);
+            Vector2 v = myBod.velocity;
+            v.x = h * 5;
+            myBod.velocity = v;
+            myRend.flipX = false;
+            myAnim.speed = 3;
+        }
+        else if (h < 0)
+        {
+            myAnim.SetBool("Run", true);
+            Vector2 v = myBod.velocity;
+            v.x = h * 5;
+            myBod.velocity = v;
+            myRend.flipX = true;
+            myAnim.speed = 3;
+        }
+        else
+        {
+            myAnim.SetBool("Run", false);
+        }
+    }
+}
